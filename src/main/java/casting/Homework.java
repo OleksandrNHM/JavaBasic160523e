0b401c6db9e2db186946b092885bd82b0ebec06f
OleksandrNHM <shura.nayhum0@gmail.com>
2023-06-11 14:19:17 +0200

    Homework

---
+++ src/main/java/casting/Homework.java
@@ -0,0 +1,16 @@
+package casting;
+
+public class Homework {
+    public static void main(String[] args) {
+        float q = (float) (Math.pow(Math.E, 5) + 1.5);
+        System.out.println(q);
+
+        byte w = (byte) (Math.log(2) + (short) 14);
+        System.out.println(w);
+
+        double e = 1 + 1.5;
+        System.out.println(e);
+
+
+    }
+}
