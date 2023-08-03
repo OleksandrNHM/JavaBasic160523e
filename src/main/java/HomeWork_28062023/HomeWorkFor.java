27177f01394b2016e726ffcd6c96e9e4cd08f5ca
OleksandrNHM <shura.nayhum0@gmail.com>
2023-08-03 21:47:10 +0200

    Homework

---
+++ src/main/java/HomeWork_28062023/HomeWorkFor.java
@@ -0,0 +1,12 @@
+package HomeWork_28062023;
+
+public class HomeWorkFor {
+    public static void main(String[] args) {
+        int currentNumber = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(currentNumber + " ");
+            currentNumber *= 2;
+        }
+    }
+}
+
